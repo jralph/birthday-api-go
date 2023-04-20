82a6d5b1067ee29cf2c4afab4a3fd39575728d6a
Joseph Ralph <j.ralph@gymshark.com>
2023-04-20 17:23:45 +0100

    Updating util comments.

--- internal/utils/envConfig.go
+++ internal/utils/envConfig.go
@@ -7,12 +7,14 @@ import (
 	"time"
 )
 
+// OverrideFromEnvStr will replace the given string pointer with the given env value
 func OverrideFromEnvStr(str *string, envName string) {
 	if envValue := os.Getenv(envName); envValue != "" {
 		*str = envValue
 	}
 }
 
+// OverrideFromEnvInt will replace the given int pointer with the given env value, converted to an int
 func OverrideFromEnvInt(i *int, envName string) {
 	if envValue := os.Getenv(envName); envValue != "" {
 		envValueInt, err := strconv.Atoi(envValue)
@@ -23,6 +25,7 @@ func OverrideFromEnvInt(i *int, envName string) {
 	}
 }
 
+// OverrideFromEnvDuration will replace the given duration pointer with the given env value, parsed as a duration
 func OverrideFromEnvDuration(duration *time.Duration, envName string) {
 	if envValue := os.Getenv(envName); envValue != "" {
 		parsedDuration, err := time.ParseDuration(envValue)
